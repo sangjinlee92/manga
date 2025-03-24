280f9b3506769ac3dd5c7f6efe8c5e02970c75c7
sangjinlee92 <krnfx.17@gmail.com>
2025-03-24 00:27:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowRight") {
+        window.location.href = "capitulo2.html"; // Ir al siguiente capítulo
+    }
+});
